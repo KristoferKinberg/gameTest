d9f97130ef8dfa7ea54c1c6c6ae97530f82cd5d5
kristofer kinberg <kristofer.kinberg@ingka.com>
2022-09-25 19:12:48 +0200

    fix broken player growth

--- src/ECS/systems/playerFoodCollisionSystem.ts
+++ src/ECS/systems/playerFoodCollisionSystem.ts
@@ -9,19 +9,29 @@ const playerFoodCollisionSystem = ({ entities }: ISystemParams) =>
 
     if (!isPLayerMovable) return;
 
-    const playerGraphics = entity.getComponent(componentTypes.GRAPHICS).getGraphicsObject();
+    const playerGraphicsComponent = entity.getComponent(componentTypes.GRAPHICS);
+    const playerGraphicsObject = playerGraphicsComponent.getGraphicsObject();
     const entitiesWithEatableComponent = entityManager.getEntitiesByComponents([componentTypes.EATABLE]);
 
     const playEatSound = () => new Audio('../../../sounds/onEat.wav').play();
+
+    const resizePlayerGraphics = () => {
+      const previousRadius = playerGraphicsObject.width / 2;
+
+      playerGraphicsObject.clear();
+      playerGraphicsObject.beginFill(0xffffff);
+      playerGraphicsObject.drawCircle(0 ,0, previousRadius + 2);
+      playerGraphicsObject.endFill();
+    };
 
     entitiesWithEatableComponent
       .forEach((eatable: any) => {
         const { isMounted, getGraphicsObject } = eatable.getComponent(componentTypes.GRAPHICS);
 
-        if (isMounted() && didCollide(getGraphicsObject(), playerGraphics)) {
+        if (isMounted() && didCollide(getGraphicsObject(), playerGraphicsObject)) {
           const scoreComponent = entity.getComponent(componentTypes.SCORE);
           getGraphicsObject().destroy();
-
+          resizePlayerGraphics();
           scoreComponent.setScore(scoreComponent.getScore() + 1);
           //playEatSound();
           eatable.destroy();
